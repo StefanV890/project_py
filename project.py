5a4f863c32e16572c7e113554a7270180730f810
StefanV890 <csgos245@gmail.com>
2025-01-31 21:14:21 +0200

    version#2

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import random
 
 def start_game():
+    guesses = []
     secret_number =  random.randint(1,100)
     tries = 0
     print("chose a number between 1-100")
@@ -8,11 +9,13 @@ def start_game():
     while True:
         tries +=1
         key_number =int(input("type a number "))
+        guesses.append(key_number)
         if secret_number < key_number:
             print("type a smaller number next time")
         elif secret_number > key_number:
             print("type a bigger number next time")
         else:
+            print("your guesses are ", guesses )
             print("good work you got it right :)")
             break
 start_game()
