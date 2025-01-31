b4ed6cb402b87f04b16a29fe55fd715d69ba6b26
StefanV890 <csgos245@gmail.com>
2025-01-31 23:30:42 +0200

    Version#4

--- project.py
+++ project.py
@@ -1,11 +1,14 @@
 import random
 
-def start_game():
+punctaj={}
+
+def start_game(name):
+    print("Start game for ",name)
     guesses = []
     secret_number =  random.randint(1,100)
     tries = 0
     print("chose a number between 1-100")
-   # print (secret_number) ###debug
+    #print (secret_number) #debug
     while True:
         tries +=1
         key_number =int(input("type a number "))
@@ -16,6 +19,28 @@ def start_game():
             print("type a bigger number next time")
         else:
             print("your guesses are ", guesses )
-            print("good work you got it right :)")
+            print("good work ",name,". You got it right in", tries,"attemps")
+            print("")
+            punctaj[name]=tries
             break
-start_game()
+
+user_number =int(input("how many users "))
+for i in range(user_number):
+    tx = "user name " + str(i+1) + ": "
+    a = input(tx)
+    punctaj[a]=0
+
+for i in punctaj.keys():
+    print("")
+    start_game(i)
+
+print("RESULTS")
+for i in punctaj.keys():
+    print(i,"  -  ",punctaj[i]," attemps")
+
+
+"""
+cati playeri sunt (player1:nume player2:nume) start player, start player2...
+mantin playeri si 
+
+"""
