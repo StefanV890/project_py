f2a697a5aa2c8f4b3b9f3468ae93e88d914ac860
StefanV890 <csgos245@gmail.com>
2025-02-06 15:38:25 +0200

    version 5.1

--- project.py
+++ project.py
@@ -1,20 +1,23 @@
+
+
 import random
 import time
 
-punctaj={}
-timp={}
+punctaj = {}
+timp = {}
+
 
 def start_game(name):
     t1 = int(time.time())
-    print("Start game for ",name)
+    print("Start game for ", name)
     guesses = []
-    secret_number =  random.randint(1,100)
+    secret_number = random.randint(1, 100)
     tries = 0
     print("chose a number between 1-100")
-    print (secret_number) #debug
+    print(secret_number)  # debug
     while True:
-        tries +=1
-        key_number =int(input("type a number "))
+        tries += 1
+        key_number = int(input("type a number "))
         guesses.append(key_number)
         if secret_number < key_number:
             print("type a smaller number next time")
@@ -22,20 +25,21 @@ def start_game(name):
             print("type a bigger number next time")
         else:
             t2 = int(time.time())
-            #print(t2-t1)
-            print("your guesses are ", guesses )
-            print("good work ",name,". You got it right in", tries,"attemps")
+            # print(t2-t1)
+            print(f"Your guesses: {guesses}")
+            print(f"Good work {name}. You got it right in {tries} attempts\n")
             print("")
-            punctaj[name]=tries
-            timp[name]=t2-t1
+            punctaj[name] = tries
+            timp[name] = t2 - t1
             break
 
-user_number =int(input("how many users "))
+
+user_number = int(input("how many users "))
 for i in range(user_number):
-    tx = "user name " + str(i+1) + ": "
+    tx = "user name " + str(i + 1) + ": "
     play = input(tx)
-    punctaj[play]=0
-    timp[play]=0
+    punctaj[play] = 0
+    timp[play] = 0
 
 for player in punctaj.keys():
     print("")
@@ -43,5 +47,5 @@ for player in punctaj.keys():
 
 print("RESULTS")
 for player in punctaj.keys():
-    print(player,"  -  ",punctaj[player]," attemps in ",timp[player]," sec.")
+    print(f"{player} - {punctaj[player]} attemps in {timp[player]} seconds")
 
