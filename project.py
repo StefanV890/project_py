6c1b3f6e2f36a98ed3411f43d3279e479e69010a
StefanV890 <csgos245@gmail.com>
2025-02-06 18:32:19 +0200

    Version#\5.2

--- project.py
+++ project.py
@@ -1,5 +1,3 @@
-
-
 import random
 import time
 
@@ -7,31 +5,50 @@ points = {}
 time_ = {}
 
 
+def initalize_players(user_number):
+    """
+        Function for registering players
+        ------
+        Parameters:
+        user_number(int) the number of the users
+        ------
+        Returns
+        -initial player names
+        -the index of the player
+        -crates the points and time_ dictionary
+    """
+    for i in range(user_number):
+        tx = "user name " + str(i+1) + ": "
+        play = input(tx)
+        points[play] = 0
+        time_[play] = 0
+        return points, time_
+
 def start_game(name):
     """
-    Function for starting game
-    ------
-    Parameters:
-    name(str) the name of the user WHICH IS CURRENTLY PLAYING
-    ------
-    Returns
-    -number of guesses
-    -name of te player which was playing last
-    -number of attempts
-    ------
-    Notes
-    -uncomment line 31 for debug
-    """
+        Function for starting game
+        ------
+        Parameters:
+        name(str) the name of the user WHICH IS CURRENTLY PLAYING
+        ------
+        Returns
+        -number of guesses
+        -name of te player which was playing last
+        -number of attempts
+        ------
+        Notes
+        -uncomment line 48 for debug
+        """
     t1 = int(time.time())
-    print("Start game for ", name)
+    print("Start game for ",name)
     guesses = []
-    secret_number = random.randint(1, 100)
+    secret_number =  random.randint(1,100)
     tries = 0
     print("chose a number between 1-100")
-    #print(secret_number) #debug
+    #print (secret_number) #debug
     while True:
-        tries += 1
-        key_number = int(input("type a number "))
+        tries +=1
+        key_number =int(input("type a number "))
         guesses.append(key_number)
         if secret_number < key_number:
             print("type a smaller number next time")
@@ -39,20 +56,14 @@ def start_game(name):
             print("type a bigger number next time")
         else:
             t2 = int(time.time())
+            #print(t2-t1)
             print(f"Your guesses: {guesses}")
             print(f"Good work {name}. You got it right in {tries} attempts\n")
-            print("")
-            points[name] = tries
-            time_[name] = t2 - t1
+            points[name]=tries
+            time_[name]=t2-t1
             break
-
-
 user_number = int(input("how many users "))
-for i in range(user_number):
-    tx = "user name " + str(i + 1) + ": "
-    play = input(tx)
-    points[play] = 0
-    time_[play] = 0
+initalize_players(user_number)
 
 for player in points.keys():
     print("")
@@ -60,5 +71,4 @@ for player in points.keys():
 
 print("RESULTS")
 for player in points.keys():
-    print(f"{player} - {points[player]} attempts in {time_[player]} seconds")
-
+    print(f"{player} - {points[player]} attemps in {time_[player]} seconds")
