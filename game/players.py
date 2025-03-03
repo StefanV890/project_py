2633daed2c088e0ee3c7cfcbfb5d59172736e953
StefanV890 <csgos245@gmail.com>
2025-03-03 21:17:32 +0200

    IMPLEMENTED OOP

--- game/players.py
+++ game/players.py
@@ -3,6 +3,15 @@ import time
 
 points = {}
 time_ = {}
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.attempts = 0
+        self.time_taken = 0.0
+
+    def update_score(self, attempts: int, time_taken: float):
+        self.attempts = attempts
+        self.time_taken = time_taken
 
 def initialize_players(user_number):
     """
@@ -16,11 +25,11 @@ def initialize_players(user_number):
         -the index of the player
         -crates the points and time_ dictionary
     """
-    for _ in range(user_number):
+    players = []
+    for i in range(user_number):
         tx = "user name " + str(i+1) + ": "
         play = input(tx)
         points[play] = 0
         time_[play] = 0
-    return points, time_
-
+    return points, time_, players
 
